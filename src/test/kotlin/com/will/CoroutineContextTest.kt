8f2d8732fab13f06db3715d4aee30d3ae8a793e2
seungho-kang <will.seungho@webtoonscorp.com>
2022-10-03 03:34:26 +0900

    Coroutine Context, Dispatcher

---
+++ src/test/kotlin/com/will/CoroutineContextTest.kt
@@ -0,0 +1,48 @@
+package com.will
+
+import kotlinx.coroutines.*
+import org.junit.jupiter.api.Test
+
+/**
+ * Coroutines always execute in some context represented by a value of the CoroutineContext type.
+ *
+ * Dispatchers and threads
+ */
+internal class CoroutineContextTest {
+
+    @Test
+    fun `Dispatchers and threads`(): Unit = runBlocking {
+        launch {
+            println("main runBlocking thread: ${Thread.currentThread().name}")
+        }
+
+        launch(Dispatchers.Unconfined) {
+            println("Unconfined thread: ${Thread.currentThread().name}")
+        }
+
+        launch(Dispatchers.Default) {
+            println("Default thread: ${Thread.currentThread().name}")
+        }
+    }
+
+    /**
+     * runBlocking and coroutineScope builders both wait for their body and all its children to complete.
+     *
+     * The main difference is
+     * that the runBlocking method blocks the current thread for waiting,
+     * while coroutineScope just suspends, releasing the underlying thread for other usage
+     */
+    @Test
+    fun `Scope Builder`() = runBlocking {
+        clock()
+    }
+
+    private suspend fun clock() = coroutineScope {
+        launch {
+            delay(timeMillis = 1000L)
+            println("World")
+        }
+        println("Hello")
+    }
+
+}
